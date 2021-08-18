a9efee3b6710c11a2c735b63be19c1bbb44b67b3
Dogukan Celik <dogukancelikcse@gmail.com>
2021-08-18 18:46:25 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+file = open("test.py", "w")
+
+file.write("print('THE NUMBER IS ODD OR EVEN?')\n")
+
+file.write("number = input('Enter a number: ')\n")
+
+file.write("if number == 1: print('number is odd')\n")
+
+for i in range(2, 100000):
+    if i % 2 == 0:
+        file.write("elif number == " + str(i) + ": print('number is even')\n")
+    else:
+        file.write("elif number == " + str(i) + ": print('number is odd')\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
